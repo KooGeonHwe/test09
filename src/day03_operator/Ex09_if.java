fca8c784fb0d7f3fed202c9772c1d9bc2e885f28
Koo Geonhwe <mashrjs@naver.com>
2021-07-27 12:57:59 +0900

    연산자공부

--- src/day03_operator/Ex09_if.java
+++ src/day03_operator/Ex09_if.java
@@ -1,5 +1,14 @@
 package day03_operator;
 
-public class Ex09_if {
+import java.util.Scanner;
 
+public class Ex09_if {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("수 입력");
+		num = input.nextInt();
+		if(num >10 )
+			System.out.println("종속 문장 실행");
+	}
 }
